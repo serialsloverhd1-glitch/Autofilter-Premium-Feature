50408a8e21da4c1fc0381f6fed01de9688179043
serialsloverhd1-glitch <serialsloverhd1@gmail.com>
2026-01-13 15:27:06 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -224,7 +224,7 @@ Nᴀᴍᴇ : {}
 
 Mᴇꜱꜱᴀɢᴇ : <b>{}</b>"""
     
-    CAPTION = """<b><a href="https://t.me/iP_Update">{file_name}</a></b>\n\n<b>⚜️ Powered By : <a href="https://t.me/cinemapopkornupdates">[ iP Update ]</a></b>"""
+    CAPTION = """<b><a href="https://t.me/cinemapopkornupdates">{file_name}</a></b>\n\n<b>⚜️ Powered By : <a href="https://t.me/cinemapopkornupdates">[ iP Update ]</a></b>"""
 
     
     MOVIE_UPDATE_NOTIFY_TXT = """
